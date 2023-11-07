9aab2b0380ac68606303cdd7e0868cd42ba60304
toussaint10 <l.tona@alustudent.com>
2023-11-07 07:24:25 -0800

    Python - import & modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-   for i in sorted(dir(hidden_4)):
-        if i[:2] != "__":
-            print("{}".format(i))
+
+   for name in sorted(dir(hidden_4)):
+        if name[:2] != "__":
+            print("{}".format(name))
