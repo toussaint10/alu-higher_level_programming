06ccc2e644d43e466edf23a2ef40b1d4e41cc78a
toussaint10 <l.tona@alustudent.com>
2023-11-07 07:12:33 -0800

    Python - import & modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__name__":
-    """Print All Hidden Directories"""
-    import hidden_4
-
-    for i i dir(hidden_4):
+   for i i dir(hidden_4):
         if i[:2] != "__":
             print(i)
