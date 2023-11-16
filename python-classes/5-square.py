cec110a995f4147d53dca417af57dea77345e37c
toussaint10 <l.tona@alustudent.com>
2023-11-16 05:12:14 -0800

    Python - Classes and Objects

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,7 +20,7 @@ class Square:
         return (self.__size)
 
     @size.setter
-    def size(self,value):
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
