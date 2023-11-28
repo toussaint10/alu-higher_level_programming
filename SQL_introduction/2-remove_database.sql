07de41a58bbb3c6aefa169964c4411d8a32672be
toussaint10 <l.tona@alustudent.com>
2023-11-28 02:42:21 -0800

    SQL - Introduction

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+0-list_databases.sql
+-- Deletes the database hbtn_0c_0 from my MySQL server.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+0-list_databases.sql
+-- Lists all tables of a database on my MySQL server.
+SHOW TABLES;
