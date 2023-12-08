6f96d725ba89efd70732fbaa8800d6795b05c88e
toussaint10 <l.tona@alustudent.com>
2023-12-08 02:54:17 -0800

    Python - import & modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    """Print all names defined by hidden_4 module"""
+if __name__ == "__main__":
+    """Print all hidden directories"""
     import hidden_4
 
-    names = dir(hidden_4)
-   for name in names:
-       if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] !="__":
+            print(i)
